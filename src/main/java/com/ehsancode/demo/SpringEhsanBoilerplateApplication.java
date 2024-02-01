1b8d3829a3bfb5d58e3ec333a902027d2c302005
ehsanshavandi <ehsan.shavandi@gmail.com>
2024-02-02 00:25:50 +0330

    Refactor the name

---
+++ src/main/java/com/ehsancode/demo/SpringEhsanBoilerplateApplication.java
@@ -0,0 +1,12 @@
+package com.ehsancode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEhsanBoilerplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringEhsanBoilerplateApplication.class, args);
+  }
+}
